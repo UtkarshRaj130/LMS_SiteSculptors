bb7f15fcf75ca59a099b4035da5c2bd88cc654c4
RMan2323 <raghuveer23verma@gmail.com>
2024-07-06 01:57:50 +0530

    Added IITDh logo (with iitdh.ac.in link) and made the color scheme similar to our institute's website

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,55 +1,44 @@
-import { React, useState } from 'react'
-import './Header.css'
+import React, { useState } from 'react';
+import './Header.css';
+import logo from './IIT_Dharwad_Emblem.svg.png'; // Importing the logo image
 
 function Header() {
- 
-const [menutoggle, setMenutoggle] = useState(false)
+    const [menutoggle, setMenutoggle] = useState(false);
 
-// const Toggle = () => {
-//     setMenutoggle(!menutoggle)
-// }
+    const closeMenu = () => {
+        setMenutoggle(false);
+    }
 
-const closeMenu = () => {
-    setMenutoggle(false)
+    return (
+        <div className="header">
+            <div className="logo-nav">
+                <a href="https://iitdh.ac.in">
+                    <img src={logo} alt="College Logo" className="college-logo" />
+                </a>
+                <a href="/home" className="library-text">LIBRARY</a>
+            </div>
+            <div className='nav-right'>
+                <input className='search-input' type='text' placeholder='Search here for Books...' />
+                <ul className={menutoggle ? "nav-options active" : "nav-options"}>
+                    <li className="option" onClick={() => {closeMenu()}}>
+                        <a href='/'>
+                            Home
+                        </a>
+                    </li>
+                    <li className="option" onClick={() => {closeMenu()}}>
+                        <a href='/books'>
+                            Books
+                        </a>
+                    </li>
+                    <li className="option" onClick={() => {closeMenu()}}>
+                        <a href='/signin'>
+                            Sign In
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
-
-return (
-    <div className="header">
-    <div className="logo-nav">
-    <a href='/'>
-        <a href="/home">LIBRARY</a>
-    </a>
-    </div>
-    <div className='nav-right'>
-        <input className='search-input' type='text' placeholder='Search here for Books...'/>
-        <ul className={menutoggle ? "nav-options active" : "nav-options"}>
-            <li className="option" onClick={() => {closeMenu()}}>
-                <a href='/'>
-                    <a href="/home">Home</a>
-                </a>
-            </li>
-            <li className="option" onClick={() => {closeMenu()}}>
-                <a href='/books'>
-                <a href="/books">Books</a>
-                </a>
-            </li>
-            <li className="option" onClick={() => {closeMenu()}}>
-                <a href='/signin'>
-                <a href='/signin'>SignIn</a>
-                </a>
-            </li>
-        </ul>
-    </div>
-    <hr style={{color:"white"}}/>
-    </div>
-
-)
-  
-
-   
-
-   
-}
-
-export default Header
+export default Header;
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -8,6 +8,7 @@ function Home() {
     <div>
     <Header/>
     <ImgSlide/>
+    <hr className="divider" />
     <About/>
 
     </div>
