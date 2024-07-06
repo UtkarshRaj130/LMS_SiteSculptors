28cc879b014094c0ca987004713ff93a4d559100
nidhi-005 <cs23bt060@iitdh.ac.in>
2024-07-07 02:22:13 +0530

    Update bookControllers.js

--- backend/bookControllers.js
+++ backend/bookControllers.js
@@ -1,45 +1,74 @@
 import Book from './Book.js';
 
+// Create a new book
+const createBook = async (req, res) => {
+  const { title, author, genre } = req.body;
+
+  const book = new Book({
+    title,
+    author,
+    genre,
+  });
+
+  const createdBook = await book.save();
+  res.status(201).json(createdBook);
+};
+
 // Get all books
 const getBooks = async (req, res) => {
-  try {
-    const books = await Book.find({});
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  const books = await Book.find({});
+  res.json(books);
+};
+
+// Update an existing book
+const updateBook = async (req, res) => {
+  const { title, author, genre, borrowed } = req.body;
+
+  const book = await Book.findById(req.params.id);
+
+  if (book) {
+    book.title = title;
+    book.author = author;
+    book.genre = genre;
+    book.borrowed = borrowed;
+
+    const updatedBook = await book.save();
+    res.json(updatedBook);
+  } else {
+    res.status(404).json({ message: 'Book not found' });
+  }
+};
+
+// Delete a book
+const deleteBook = async (req, res) => {
+  const book = await Book.findById(req.params.id);
+
+  if (book) {
+    await book.remove();
+    res.json({ message: 'Book removed' });
+  } else {
+    res.status(404).json({ message: 'Book not found' });
   }
 };
 
 // Search for books
 const searchBooks = async (req, res) => {
   const { query } = req.query;
-  try {
-    const books = await Book.find({ $text: { $search: query } });
-    res.json(books);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+
+  if (!query) {
+    return res.status(400).json({ message: 'Query parameter is required' });
   }
+
+  const books = await Book.find({
+    $or: [
+      { title: { $regex: query, $options: 'i' } },
+      { author: { $regex: query, $options: 'i' } },
+      { genre: { $regex: query, $options: 'i' } },
+    ],
+  });
+
+  res.json(books);
 };
 
-// Update book availability
-const updateBook = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const book = await Book.findById(id);
-    if (book) {
-      book.available = !book.available;
-      await book.save();
-      res.json(book);
-    } else {
-      res.status(404).json({ message: 'Book not found' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+export { createBook, getBooks, updateBook, deleteBook, searchBooks };
 
-export {
-  getBooks,
-  searchBooks,
-  updateBook,
-};
