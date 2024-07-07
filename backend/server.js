0e8de3104ade225e2205fcc66a9fe900a2750b94
nidhi-005 <cs23bt060@iitdh.ac.in>
2024-07-07 12:51:35 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,6 @@ app.use(express.json());
 
 app.use('/api/books', bookRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
