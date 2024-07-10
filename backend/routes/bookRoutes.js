91732e1690ee7301c525207cdd4e301c494d4d3b
UtkarshRaj130 <utkarshraj130@gmail.com>
2024-07-11 01:14:29 +0530

    Deleted all the content of main branch

--- backend/routes/bookRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllBooks, addBook } from '../controllers/bookController.js';
-
-const router = express.Router();
-
-router.get('/', getAllBooks);
-router.post('/', addBook);
-
-export default router;
